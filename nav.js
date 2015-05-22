91db28119333912621717963cba942f5019c5e52
Tanner Hodges <austintannerhodges@gmail.com>
2015-05-22 11:04:23 -0400

    Further layman-izing (named vars + explicit if/else)

--- nav.js
+++ nav.js
@@ -1,93 +1,138 @@
 (function() {
     'use strict';
 
+    /**
+     * Is nav small?
+     * @return {Boolean}
+     */
     function isNavSmall() {
-        return 0 === $(".nav-list_inline .nav--item").length;
+        return 0 === $('.nav-list_inline .nav--item').length;
     }
 
+    /**
+     * Navigation resize handler
+     * @return {void}
+     */
     function navigationResizeHandler() {
-        var b, c, d, e;
-        e = $(".nav-list_inline").width();
-        c = 0;
+        var $lastNavItem,
+            sumOfNavItemWidths,
+            navToggleText,
+            navListInlineWidth;
 
-        $(".nav-list_inline .nav--item").each(function(a, b) {
-            return c += $(b).width();
+        navListInlineWidth = $('.nav-list_inline').width();
+        sumOfNavItemWidths = 0;
+
+        $('.nav-list_inline .nav--item').each(function(a, b) {
+            return sumOfNavItemWidths += $(b).width();
         });
 
-        $(".nav-list .nav--item").each(function(a, b) {
-            return $(b).hasClass("nav--item_about") ? !1 : (
-                $(b).appendTo(".nav-list_inline"),
-                $(b).width() + c > e ? (
-                    $(b).prependTo(".nav-list"),
-                    !1
-                ) : c = $(b).width() + c
-            )
+        $('.nav-list .nav--item').each(function(a, b) {
+            if ($(b).hasClass('nav--item_about')) {
+                return false;
+            }
+
+            $(b).appendTo('.nav-list_inline');
+
+            if ($(b).width() + sumOfNavItemWidths > navListInlineWidth) {
+                $(b).prependTo('.nav-list');
+                return false;
+            }
+
+            sumOfNavItemWidths = $(b).width() + sumOfNavItemWidths;
         });
 
-        b = $(".nav-list_inline .nav--item:last-child");
+        $lastNavItem = $('.nav-list_inline .nav--item:last-child');
 
-        b.width() + c > e && b.prependTo(".nav-list");
+        if ($lastNavItem.width() + sumOfNavItemWidths > navListInlineWidth) {
+            $lastNavItem.prependTo('.nav-list');
+        }
 
-        "inline" === $(".nav-list .nav--item:last-child").css("display") && $(".nav-list_inline .nav--item").each(function(a, b) {
-            return $(b).insertBefore(".nav--item_about")
-        });
+        if ('inline' === $('.nav-list .nav--item:last-child').css('display')) {
+            $('.nav-list_inline .nav--item').each(function(a, b) {
+                return $(b).insertBefore('.nav--item_about');
+            });
+        }
 
-        d = isNavSmall() ? "Menu" : $(".nav-list").hasClass("nav_closed") ? "More" : "Less";
+        if (isNavSmall()) {
+            navToggleText = 'Menu';
+        } else if ($('.nav-list').hasClass('nav_closed')) {
+            navToggleText = 'More';
+        } else {
+            navToggleText = 'Less';
+        }
 
-        $(".nav-toggle .nav-toggle--text").text(d);
+        $('.nav-toggle .nav-toggle--text').text(navToggleText);
     }
 
+
+    /**
+     * Navigation
+     * @return {void}
+     */
     function navigation() {
-        var b;
+        var $navList;
 
         navigationResizeHandler();
 
-        b = $(".nav-list");
+        $navList = $('.nav-list');
+        $navList.addClass('nav_closed');
 
-        b.addClass("nav_closed");
+        $('.member-tab').addClass('member-tab_hidden');
 
-        $(".member-tab").addClass("member-tab_hidden");
+        if (!isNavSmall()) {
+            $('.nav-toggle .nav-toggle--text').text('More');
+        }
 
-        isNavSmall() || $(".nav-toggle .nav-toggle--text").text("More");
+        $('.nav-toggle').on('click', function(event) {
+            var navToggleText;
 
-        $(".nav-toggle").on("click", function(a) {
-            var c;
+            event.preventDefault();
 
-            a.preventDefault();
-
-            if (b.hasClass("nav_closed")) {
-                c = "Less";
-                $(this).addClass("nav-toggle_open")
-                    .children(".icon-arrow")
-                    .addClass("icon-arrow-gold")
-                    .removeClass("icon-arrow-down-white");
-                $(".nav").removeClass("closed");
-                b.removeClass("nav_closed");
-                $(".member-tab").removeClass("member-tab_hidden");
+            if ($navList.hasClass('nav_closed')) {
+                navToggleText = 'Less';
+                $(this).addClass('nav-toggle_open')
+                    .children('.icon-arrow')
+                    .addClass('icon-arrow-gold')
+                    .removeClass('icon-arrow-down-white');
+                $('.nav').removeClass('closed');
+                $navList.removeClass('nav_closed');
+                $('.member-tab').removeClass('member-tab_hidden');
             } else {
-                c = "More";
-                $(this).removeClass("nav-toggle_open")
-                    .children(".icon-arrow")
-                    .addClass("icon-arrow-down-white")
-                    .removeClass("icon-arrow-gold");
+                navToggleText = 'More';
+                $(this).removeClass('nav-toggle_open')
+                    .children('.icon-arrow')
+                    .addClass('icon-arrow-down-white')
+                    .removeClass('icon-arrow-gold');
 
-                $(".nav").addClass("closed");
-                b.addClass("nav_closed");
-                $(".member-tab").addClass("member-tab_hidden");
+                $('.nav').addClass('closed');
+                $navList.addClass('nav_closed');
+                $('.member-tab').addClass('member-tab_hidden');
             }
         });
     }
 
+    /**
+     * Home nav check
+     * @return {void}
+     */
     function homeNavCheck() {
-        $(".home .header").addClass("transparent");
-        $(".home .nav").addClass("transparent");
+        $('.home .header').addClass('transparent');
+        $('.home .nav').addClass('transparent');
     }
 
+    /**
+     * Home nav check
+     * @return {void}
+     */
     function initHandlers() {
         $(window).resize(_.throttle(navigationResizeHandler, 50));
         $(window).trigger('resize');
     }
 
+    /**
+     * Home nav check
+     * @return {void}
+     */
     function init() {
         navigation();
         homeNavCheck();
